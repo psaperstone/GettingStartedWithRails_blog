e8e640dd162449f4ec33d5e6349bd4c3b266d77e
Paul Saperstone <paul@saperstone.com>
2016-05-08 16:38:55 -0400

    Getting Started with Rails : Blog example

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+# Homework Assignment: do the Rails Getting Started Guide
+# http://guides.rubyonrails.org/getting_started.html
+
+class WelcomeController < ApplicationController
+  def index
+  end
+end
